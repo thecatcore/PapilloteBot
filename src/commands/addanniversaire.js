f4af9998c7054aa8193b292e408e1325112c2796
arthurbambou <34719527+arthurbambou@users.noreply.github.com>
2017-12-23 19:08:08 +0100

    Update addanniversaire.js

--- src/commands/addanniversaire.js
+++ src/commands/addanniversaire.js
@@ -1,5 +1,14 @@
-const addanniversaire () => {
+const addanniversaire = () => {
+  const date = message.content.substr(17);
+  const annivperso = message.author.toString();
 
-}
+  console.log(value);
+  message.reply("Ajout de la citation à la base de données")
 
-module.export = addanniversaire
+  db.addAnniversaire({
+    anniv_date: date,
+    anniv_annivperso: annivperso
+});
+};
+
+module.exports = addanniversaire
