b5f0b169a1b93e563fe7d8a3c1b82bb6f80fdfbc
Arthur Roussel <arthur.koalas@free.fr>
2018-01-19 23:56:31 +0100

    fix undefined

--- src/commands/overwatch_info.js
+++ src/commands/overwatch_info.js
@@ -4,17 +4,19 @@ const info_player = (message) => {
 
 //// Search for a player ( you must have the exact username, if not Blizzard api will return a not found status)
 owjs
-    .search('Zeya#2303')
-    .then((data) => {
-        console.log(data);
+    .getAll('pc', 'eu', 'Zeya-2303')
+    .then((data) => {console.dir(data, {depth : 2, colors : true})
         var ov_info_embed = new Discord.RichEmbed()
-        .setTitle(data.platformDisplayName)
-        .addField('Niveau', data.level)
-        .setImage(data.portrait)
-        .addField('Tier', data.tier)
-        .addField('Platforme', data.region);
+        .setTitle(data.profile.nick)
+        .addField('Niveau', data.profile.level)
+        .setThumbnail(data.profile.avatar)
+        .addField('Tier', data.profile.ranking)
+        .addField('Rank', data.profile.rank);
         message.channel.send(ov_info_embed); 
     });
+    owjs
+    .getAll('pc', 'eu', 'Zeya-2303')
+    .then((data) => console.dir(data, {depth : 2, colors : true}) );
 }
 
 module.exports = info_player;
