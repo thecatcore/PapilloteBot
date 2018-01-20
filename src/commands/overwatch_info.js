6ee2e17dc15b794abaa1d5d8d0bcfab361798cda
Arthur Roussel <arthur.koalas@free.fr>
2018-01-20 10:49:15 +0100

    ov_info command function !

--- src/commands/overwatch_info.js
+++ src/commands/overwatch_info.js
@@ -1,11 +1,11 @@
 const info_player = (message) => {
     var owjs = require('overwatch-js');
     var Discord = require('discord.js');
-
-//// Search for a player ( you must have the exact username, if not Blizzard api will return a not found status)
-owjs
-    .getAll('pc', 'eu', 'Zeya-2303')
-    .then((data) => {console.dir(data, {depth : 2, colors : true})
+    const value = message.content.substr(9);
+    console.log(value);
+    owjs.getAll('pc', 'eu', `${value}`)
+    .then((data) => {
+        //console.dir(data, {depth : 2, colors : true})
         var ov_info_embed = new Discord.RichEmbed()
         .setTitle(data.profile.nick)
         .addField('Niveau', data.profile.level)
@@ -13,10 +13,12 @@ owjs
         .addField('Tier', data.profile.ranking)
         .addField('Rank', data.profile.rank);
         message.channel.send(ov_info_embed); 
-    });
-    owjs
-    .getAll('pc', 'eu', 'Zeya-2303')
-    .then((data) => console.dir(data, {depth : 2, colors : true}) );
-}
+    })
+    .catch((error) => {
+        console.error(error);
+        message.channel.send('Profile non trouvé');
+      });
+      
+};
 
 module.exports = info_player;
