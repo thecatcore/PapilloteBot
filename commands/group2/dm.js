1fc71d9886b1f8014d77070fbfbdfa95044345dd
Arthur Roussel <arthur.koalas@free.fr>
2018-01-22 18:10:34 +0100

    Add the dm command

---
+++ commands/group2/dm.js
@@ -0,0 +1,29 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class SayCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'dm',
+            group: 'group2',
+            memberName: 'dm',
+            description: 'Envoyer un message à la personne mentionnée.',
+            examples: ['dm @User Hi there!'],
+            args: [
+                {
+                    key: 'user',
+                    prompt: 'à qui souhaite-tu envoyer ce message ?',
+                    type: 'user'
+                },
+                {
+                    key: 'content',
+                    prompt: 'Quel est le message ?',
+                    type: 'string'
+                }
+            ]
+        });    
+    }
+
+    run(msg, { user, content }) {
+        return user.send(content);
+    }
+};
