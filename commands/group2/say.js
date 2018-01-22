eec172cde862ee268d76a662088159942d08912f
Arthur Roussel <arthur.koalas@free.fr>
2018-01-22 17:52:12 +0100

    Add the command say

---
+++ commands/group2/say.js
@@ -0,0 +1,25 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class SayCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'say',
+            group: 'group2',
+            memberName: 'say',
+            description: 'Le bot revoie le message écris après la commande',
+            examples: ['say Hi there!'],
+            args: [
+                {
+                    key: 'text',
+                    prompt: 'Qu\'es-ce que vous voulez faire dire au bot ?',
+                    type: 'string'
+                }
+            ]
+        });    
+    }
+
+    run(msg, { text }) {
+        msg.delete();
+        return msg.say(text);
+    }
+};
