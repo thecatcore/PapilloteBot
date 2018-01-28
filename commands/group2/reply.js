e0435e7ed3445ba47077b2bf2048a10b87172709
Arthur Roussel <arthur.koalas@free.fr>
2018-01-28 19:50:54 +0100

    Fix #19 , #20 , #21 , #22

--- commands/group2/reply.js
+++ commands/group2/reply.js
@@ -3,7 +3,7 @@ const { Command } = require('discord.js-commando');
 module.exports = class ReplyCommand extends Command {
     constructor(client) {
         super(client, {
-            name: 'reply',
+            name: "reply",
             group: 'group2',
             memberName: 'reply',
             description: 'Répond avec un Message.',
