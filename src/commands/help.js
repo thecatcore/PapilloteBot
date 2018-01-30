4c0becb463204476fd89dd223ae05d3b84eba79e
Arthur Roussel <arthur.koalas@free.fr>
2018-01-30 17:26:49 +0100

    Fix help.js

--- src/commands/help.js
+++ src/commands/help.js
@@ -8,16 +8,16 @@ const help = (db, message) => {
         .addField('Anniversaire commands', "+addanniversaire : ajoute votre date d'anniversaire à une base de donnée, \n exemple: +addanniversaire 04/01.")
         .addField('Commandes utiles', "+info : donne des infos sur le bot.\n+meteo (nom d'un lieu) : vous donne les condition actuelle du lieu en question.")
         .addField('Image commands', "+cat : vous affiche une image de chat aléatoire.")
-        .addField('Games commandes', "+osu_info {nom d'un joueur}: donne des info sur le joueur en question.\nexemple : +osu_info arthurbambou\n+ov_info {nom d'un jour}: donne beaucoup d'info sur le joueur en question, attention il faut remplacer le # par - et prendre les chiffres après le # en compte sinon ça ne marche pas.\nexemple : +ov_info ADA9-2142")
-        message.channel.send(help_embed) 
+        .addField('Games commandes', "+osu_info {nom d'un joueur}: donne des info sur le joueur en question.\nexemple : +osu_info arthurbambou\n+ov_info {nom d'un jour}: donne beaucoup d'info sur le joueur en question, attention il faut remplacer le # par - et prendre les chiffres après le # en compte sinon ça ne marche pas.\nexemple : +ov_info ADA9-2142");
+        message.channel.send(help_embed) ;
    
    
     console.log("Help command");
-    const guildname = message.guild.name
-    const guildid = message.guild.id
-    const guildregion = message.guild.region
-    console.log(guildregion)
-    console.log(message.guild.roles)
+    const guildname = message.guild.name;
+    const guildid = message.guild.id;
+    const guildregion = message.guild.region;
+    console.log(guildregion);
+    console.log(message.guild.roles);
     
     db.addGuild({
         nom: guildname,
