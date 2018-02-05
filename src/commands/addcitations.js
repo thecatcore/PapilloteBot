180f02656ff8b6bd2a03a56f1b4fee545fb9e5b1
Arthur Roussel <arthur.koalas@free.fr>
2018-02-05 18:58:47 +0100

    Fix addcitation

--- src/commands/addcitations.js
+++ src/commands/addcitations.js
@@ -2,12 +2,12 @@
 const addcitation = (db, message) => {
   const value = message.content.substr(13);
   const contributor = message.author.toString();
-  message.reply("Ajout de la citation à la base de données")
+  message.reply("Ajout de la citation à la base de données");
 
   db.addCitation({
     citation_value: value,
     citation_contributor: contributor
-  });
+  })
 };
 
 module.exports = addcitation;
