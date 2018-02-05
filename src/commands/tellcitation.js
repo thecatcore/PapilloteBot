d22d2b72b303a40cc3cec2012cb286a23f94551f
Arthur Roussel <arthur.koalas@free.fr>
2018-02-05 18:51:20 +0100

    Fix tellcitation.js

--- src/commands/tellcitation.js
+++ src/commands/tellcitation.js
@@ -1,21 +1,21 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 
-const tell_citation = (db, message, randnum) => {
+const tellCitation = (db, message, randnum) => {
         console.log(randnum);
         const citation = db.getOneCitationById(randnum);
         console.log(citation);
 
 
-        var tellcitation_embed = new Discord.RichEmbed()
-          .setColor('#D9F200')
+        var tellcitationEmbed = new Discord.RichEmbed()
+          .setColor("#D9F200")
           .setImage("https://omnilogie.fr/images/O/e239ced74cfc679e987778a89a95ebe0.jpg")
           .addField("Citation :", `${citation.citation_value}`)
           .addField("Contributeur :", `${citation.citation_contributor}`)
           .setTimestamp();
           
-        message.channel.send(tellcitation_embed);
+        message.channel.send(tellcitationEmbed);
 
         console.log(tellcitation_embed);
-}
+};
 
-module.exports = tell_citation;
+module.exports = tellCitation;
