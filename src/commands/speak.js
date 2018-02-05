ce740c42e0f8fa713ad6cbe8bbd8dd8f1c1b0c41
Arthur Roussel <arthur.koalas@free.fr>
2018-02-05 19:09:32 +0100

    Fix speak.js

--- src/commands/speak.js
+++ src/commands/speak.js
@@ -11,37 +11,37 @@ const speak = (message) => {
     }
     if (message.content === "test") {
         message.reply("Ceci est un test");
-    };
+    }
     if (message.content === "comment ça va") {
         message.reply("bah toujours en programmation donc on va dire :smiley: et toi ?");
-    };
+    }
     if (message.content === "bien") {
         message.reply("super alors !");
-    };
+    }
     if (message.content === "mal") {
         message.reply("Ah, pourquoi ?");
-    };
+    }
     if (message.content === "salut sa va ?") {
         message.reply("salut");
         message.reply("bah toujours en programmation donc on va dire :smiley: et toi ?");
-    };
+    }
     if (message.content === "nyan cat") {
         message.reply("https://www.youtube.com/watch?v=QH2-TGUlwu4");
-    };
+    }
     if (message.content === "space") {
         message.reply("https://media.licdn.com/mpr/mpr/AAEAAQAAAAAAAAdmAAAAJDIxOTUzMTY0LTg0NWItNGNkYy04NDE3LTcyZDU1NjFkYmZiZg.jpg");
         message.reply("http://i.neoseeker.com/n/9/spaaace.jpg");
-    };
+    }
 
     if (message.content === "a+") {
         message.reply("bye");
-    };
+    }
     if (message.content === "chat") {
         message.reply("https://www.youtube.com/watch?v=UTx_DF2IsFk");
-    };
+    }
     if (message.content === "chien") {
         message.reply("https://www.youtube.com/watch?v=_Vg9Z0CKPvA");
-    };
+    }
     if (message.content === "raconte moi une blague papillote") {
         let blaguenum = Math.floor(Math.random() * 4);
         if (blaguenum === 0) {
@@ -61,23 +61,23 @@ const speak = (message) => {
         }*///}
     }}}
         message.reply(blague);
-    };
+    }
     if(message.content === "bye") {
     message.reply("à plus tard");
-    };
+    }
     if(message.content === "je dois y aller") {
         message.reply("snif :sob:");
-    };
+    }
     if(message.content === "bonjour") {
         message.reply("salut :smile:");
-    };
+    }
     if(message.content === "je lag") {
         var jelagEmbed = new Discord.RichEmbed() //je créais une variable dans laquelle je stocke le message encadré
         .addField("ha sa cest un problème :sweat:", "https://www.youtube.com/watch?v=XE6YaLtctcI");// j'ajoute un espace, le premier text est le titre de cet espace et le deuxième est ça description
         message.channel.send(jelagEmbed);//et là j'envoi mon message encadré sous forme de variable :-)
         //message.reply('');
         //message.reply();
-     };
+     }
 
     };
 module.exports = speak;
