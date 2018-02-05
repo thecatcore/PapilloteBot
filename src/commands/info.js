a418e703f9fd55da29b85d716d44e9c48dddbabc
Arthur Roussel <arthur.koalas@free.fr>
2018-02-05 18:38:27 +0100

    Fix info.js

--- src/commands/info.js
+++ src/commands/info.js
@@ -1,21 +1,16 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const bot = new Discord.Client();
 
 const info = (db,message,ilink, h, m, s) => {
-    //var upTime = new Discord.Client(uptime);
     
-    //console.log(upTime)
-    
-    var info_embed = new Discord.RichEmbed()
-        .setColor('#D9F200')
+    var infoEmbed = new Discord.RichEmbed()
+        .setColor("#D9F200")
         .addField("Mon Créateur", "<@223864389256609792>")
         .addField("Prefix", "+")
         .addField("Version", "1.3")
-        .addField("Lien d'invitation", `${ilink}`)
-        //.addField("Uptime", `${s} heures ${m} minutes ${h} secondes`)
-      console.log(message)
-        message.channel.send(info_embed);
+        .addField("Lien d'invitation", `${ilink}`);
+        message.channel.send(infoEmbed);
         console.log("Info command");
-}
+};
 
 module.exports = info;
