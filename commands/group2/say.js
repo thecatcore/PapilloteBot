a8fde07fcea49fa047d0b7daebb3cb268dab2fd5
arthurbambou <34719527+arthurbambou@users.noreply.github.com>
2018-02-06 12:14:32 +0000

    Fix say.js

--- commands/group2/say.js
+++ commands/group2/say.js
@@ -1,19 +1,19 @@
-const { Command } = require('discord.js-commando');
+const { Command } = require("discord.js-commando");
 
 module.exports = class SayCommand extends Command {
     constructor(client) {
         super(client, {
-            name: 'say',
-            aliases: ['repeat', 'echo', 'parrot'],
-            group: 'group2',
-            memberName: 'say',
-            description: 'Le bot revoie le message écris après la commande',
-            examples: ['say Hi there!'],
+            name: "say",
+            aliases: ["repeat", "echo", "parrot"],
+            group: "group2",
+            memberName: "say",
+            description: "Le bot revoie le message écris après la commande",
+            examples: ["say Hi there!"],
             args: [
                 {
-                    key: 'text',
+                    key: "text",
                     prompt: "Qu'es-ce que vous voulez faire dire au bot ?",
-                    type: 'string'
+                    type: "string"
                 }
             ]
         });    
