a02507b535870357872ee6ee508f702531f097ba
arthurbambou <34719527+arthurbambou@users.noreply.github.com>
2018-02-06 12:09:28 +0000

    Fix cat.js

--- commands/group3/cat.js
+++ commands/group3/cat.js
@@ -1,23 +1,23 @@
-const { Command } = require('discord.js-commando');
-const superagent = require('superagent');
-const { RichEmbed } = require('discord.js');
+const { Command } = require("discord.js-commando");
+const superagent = require("superagent");
+const { RichEmbed } = require("discord.js");
 
 
 module.exports = class CatCommand extends Command {
     constructor(client) {
         super(client, {
-            name: 'cat',
-            group: 'group3',
-            aliases: ['chat'],
-            memberName: 'cat',
-            description: 'Le bot envoie une image de chat :-)',
-            examples: ['cat'],
+            name: "cat",
+            group: "group3",
+            aliases: ["chat"],
+            memberName: "cat",
+            description: "Le bot envoie une image de chat :-)",
+            examples: ["cat"],
         });    
     }
 
     async run(msg) {
         const { body } = await superagent
-	   .get('http://random.cat/meow');
+	   .get("http://random.cat/meow");
 	   const embed = new RichEmbed()
 	   .setColor(0x954D23)
 	   .setTitle("Meow :cat:")
