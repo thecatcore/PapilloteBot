ffd7b3af76d210a3c44edfcf07fd4b3cd430f023
Arthur Roussel <arthur.koalas@free.fr>
2018-02-07 16:42:15 +0100

    add tellcitation command

---
+++ commands/group1/tellcitation.js
@@ -0,0 +1,42 @@
+const Commando = require('discord.js-commando');
+const path = require('path');
+const { Command } = require('discord.js-commando');
+const low = require("lowdb");
+const FileSync = require('lowdb/adapters/FileSync')
+const adapter = new FileSync('citations.json')
+const db = low(adapter)
+const { RichEmbed } = require("discord.js");
+
+
+
+module.exports = class TellCitationCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'tellcitation',
+            group: 'group1',
+            memberName: 'tellcitation',
+            description: '',
+            examples: [''],
+        });    
+    }
+
+    async run(msg) {
+        var length = db.get("citations")
+                       .size()
+                       .value();
+        console.log(length);
+        var id = Math.floor(Math.random() * length);
+        console.log(id);
+        
+        var citiation = db.get(`citations[${id}].citations`)
+          .value();
+          var cont = db.get(`citations[${id}].contributeur`)
+          .value();
+          var aut = db.get(`citations[${id}].auteurs`)
+          .value();
+        const embed = new RichEmbed()
+	   .setColor(0x954D23)
+	   .setTitle("Citation :")
+	   .setDescription(`${citiation}\nde ${aut}, ajoutée par ${cont} à la base de donnée.`)
+        msg.embed(embed);
+    }}
---
+++ db.js
@@ -0,0 +1,95 @@
+const FileSync = require("lowdb/adapters/FileSync");
+const low = require("lowdb");
+
+const connect = () => {
+  const adapter = new FileSync("database.json");
+  return low(adapter);
+};
+
+const connect2 = () => {
+  const adapter1 = new FileSync("guildsinfo.json");
+  return low(adapter1);
+};
+
+const init = () => {
+  const db = connect();
+  db.defaults({ citations: [] }) 
+    .write();
+  db.defaults({ annivs: [] }) 
+    .write();
+  const db1 = connect2();
+  db1.defaults({ guilds: [] })
+     .write();
+};
+
+const getCountOfCitations = () => {
+  const db = connect();
+
+  return 
+};
+
+const getCountOfAnnivdate = () => {
+  const db = connect();
+
+  return db.get("annivs")
+    .size()
+    .value();
+};
+
+const getCountOfGuilds = () => {
+  const db = connect2();
+
+  return db.get("guilds")
+    .size()
+    .value();
+};
+/**
+ * @param {*} id identifiant de la citation
+ */
+const getOneCitationById = (id) => {
+  const db = connect();
+
+  return 
+};
+
+const getOneAnnivById = (id) => {
+  const db = connect();
+
+  return db.get(`anniv[${id}]`).value();
+};
+
+const addCitation = (citation) => {
+  const db = connect();
+
+  db.get("citations")
+    .push(citation)
+    .write();
+};
+
+const addGuild = (guild) => {
+  const db = connect2();
+
+  db.get("guilds")
+    .push(guild)
+    .write();
+};
+
+const addAnniversaire = (anniv) => {
+  const db = connect();
+
+  db.get("annivs")
+    .push(anniv)
+    .write();
+};
+
+module.exports = {
+  init,
+  getCountOfCitations,
+  getOneCitationById,
+  addCitation,
+  addAnniversaire,
+  getCountOfAnnivdate,
+  getOneAnnivById,
+  addGuild,
+  getCountOfGuilds
+};
