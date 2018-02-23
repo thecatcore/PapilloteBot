cc747951c3d0636b8bb5c382e0c1c94e8562793b
Arthur Roussel <arthur.koalas@free.fr>
2018-02-23 13:15:34 +0100

    Changement de la commande

--- commands/citations/citation add.js
+++ commands/citations/citation add.js
@@ -12,17 +12,12 @@ const { RichEmbed } = require("discord.js");
 module.exports = class AddCitationCommand extends Command {
     constructor(client) {
         super(client, {
-            name: "citation",
+            name: "citation add",
             group : "citations",
-            memberName: "citation",
+            memberName: "citation add",
             description: "",
             examples: [""],
             args: [
-                {
-                    key: "subcommand",
-                    prompt: "add ou tell",
-                    type: "string"
-                },
                 {
                     key: "citation",
                     prompt: "quelle est ta citation ?(cet argument n'est obligatoire que si vous avez mis la sous-commande << tell >>)",
@@ -38,7 +33,7 @@ module.exports = class AddCitationCommand extends Command {
     }
 
     async run(msg, { subcommand, citation, auteur }) {
-        if (subcommand === "add") {
+        
         msg.channel.send(`Ajout de la citation:\n${citation}\nde ${auteur} par ${msg.author} à la base de données.`);
         db.defaults({ citations: [] })
           .write();
@@ -48,28 +43,4 @@ module.exports = class AddCitationCommand extends Command {
             auteurs: `${auteur}`
           })
           .write();
-        } else {
-            if (subcommand === "tell") {
-                var length = db.get("citations")
-                       .size()
-                       .value();
-        console.log(length);
-        var id = Math.floor(Math.random() * length);
-        console.log(id);
-        
-        var citiation = db.get(`citations[${id}].citations`)
-          .value();
-          var cont = db.get(`citations[${id}].contributeur`)
-          .value();
-          var aut = db.get(`citations[${id}].auteurs`)
-          .value();
-        const embed = new RichEmbed()
-	   .setColor("#D9F200")
-	   .setImage("https://omnilogie.fr/images/O/e239ced74cfc679e987778a89a95ebe0.jpg")
-	   .setTitle("Citation :")
-	   .setDescription(`${citiation}\nde ${aut}, ajoutée par ${cont} à la base de donnée.`);
-        msg.embed(embed);
-            } else {
-		    msg.send(`La sous-commande ${subcommand} n'existe pas veuillez en donner une valide.`)
-        }
-    }}}
+    }}
