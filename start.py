923e0bf0f965440deb88844e3e4f3c2df57af0c2
Arthur Roussel <arthur.koalas@free.fr>
2018-03-05 18:07:29 +0100

    Prepare uptime

--- start.py
+++ start.py
@@ -1,12 +1,8 @@
-from Naked.toolshed.shell import execute_js
-
-success = execute_js('main.js')
-
 import discord
 import asyncio
 import json
 
-with open("configg.json", "r") as tokenfile:
+with open("config.1.json", "r") as tokenfile:
     token = json.load(tokenfile)
 client = discord.Client()
 ownerid1 = "223864389256609792"
@@ -26,38 +22,38 @@ async def on_ready():
     
 async def papillote_uptime():
     await client.wait_until_ready()
-     global minutes
-     minutes = 0
-     global secondes
-     secondes = 0
-     global heures
-     heures = 0
-     time['secondes'] = 0
-     time['heures'] = 0
-     time['minutes'] = 0
-     with open("uptime.json", "w") as fp:
-     json.dump(time, fp)
-     while not client.is_closed:
-         await asyncio.sleep(1)
-         time = {}
-         secondes += 1
-         time['secondes'] = secondes
-         with open("uptime.json", "w") as fp:
-         json.dump(time, fp)
-         if secondes == 60:
-            secondes = 0
-            minutes += 1
+    global minutes
+    minutes = 0
+    global secondes
+    secondes = 0
+    global heures
+    heures = 0
+    time = {}
+    time['secondes'] = 0
+    time['heures'] = 0
+    time['minutes'] = 0
+    with open("uptime.json", "w") as fp:
+        json.dump(time, fp)
+        while not client.is_closed:
+            await asyncio.sleep(1)
+            secondes += 1
             time['secondes'] = secondes
-            time['minutes'] = minutes
             with open("uptime.json", "w") as fp:
-            json.dump(time, fp)
-            if minutes == 60:
-               minutes = 0
-               hour += 1
-               time['heures'] = heures
-               time['minutes'] = minutes
-               with open("uptime.json", "w") as fp:
-               json.dump(time, fp)
+                json.dump(time, fp)
+            if secondes == 60:
+                secondes = 0
+                minutes += 1
+                time['secondes'] = secondes
+                time['minutes'] = minutes
+                with open("uptime.json", "w") as fp:
+                    json.dump(time, fp)
+                if minutes == 60:
+                    minutes = 0
+                    hour += 1
+                    time['heures'] = heures
+                    time['minutes'] = minutes
+                    with open("uptime.json", "w") as fp:
+                        json.dump(time, fp)
 
 client.loop.create_task(papillote_uptime())
 
