356430f876a2485e095a14d38e55363df911f01f
Arthur Roussel <arthur.koalas@free.fr>
2018-03-05 19:09:54 +0100

    Fix uptime command

--- commands/help/uptime.js
+++ commands/help/uptime.js
@@ -1,6 +1,4 @@
 const { Command } = require("discord.js-commando");
-const uptime = require("uptime.json");
-const { RichEmbed } = require("discord.js");
 
 module.exports = class UptimeCommand extends Command {
     constructor(client) {
@@ -8,18 +6,13 @@ module.exports = class UptimeCommand extends Command {
             name: "uptime",
             aliases: ["time"],
             memberName: "uptime",
-            group : "uptime",
-            description: "",
-            examples: [""]
+            group : "help",
+            description: "z",
+            examples: ["z"]
         });    
     }
 
     run(msg) {
-        var embed = new RichEmbed()
-            .setTitle("Uptime")
-            .addField("Secondes", uptime.secondes)
-            .addField("Minutes", uptime.minutes)
-            .addField("Heures", uptime.heures);
-        msg.embed(embed);
+        console.log("Uptime Command")
     }
 };
