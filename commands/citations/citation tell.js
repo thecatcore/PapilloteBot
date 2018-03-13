894ebddffc9f3d155760b6a538d038d0d513c629
Arthur Roussel <arthur.koalas@free.fr>
2018-03-13 16:12:03 +0100

    fix !

--- commands/citations/citation tell.js
+++ commands/citations/citation tell.js
@@ -2,15 +2,15 @@ const Commando = require("discord.js-commando");
 const path = require("path");
 const { Command } = require("discord.js-commando");
 const firebase = require("firebase");
-var config = {
-    apiKey: "AIzaSyBS7yIZr45Y1yrWBalCpO3Y2bkS5OYJQQY",
-    authDomain: "papillotebot.firebaseapp.com",
-    databaseURL: "https://papillotebot.firebaseio.com",
-    projectId: "papillotebot",
-    storageBucket: "",
-    messagingSenderId: "68194330678"
-  };
-  firebase.initializeApp(config);
+// var config = {
+//     apiKey: "AIzaSyBS7yIZr45Y1yrWBalCpO3Y2bkS5OYJQQY",
+//     authDomain: "papillotebot.firebaseapp.com",
+//     databaseURL: "https://papillotebot.firebaseio.com",
+//     projectId: "papillotebot",
+//     storageBucket: "",
+//     messagingSenderId: "68194330678"
+//   };
+//   firebase.initializeApp(config);
 var database = firebase.database();
 var ref = database.ref("citations");
 const { RichEmbed } = require("discord.js");
@@ -30,18 +30,29 @@ module.exports = class TellCitationCommand extends Command {
 
     async run(msg) {
         ref.on('value', gotData, errData);
-	function gotData(data) {
-		msg.channel.send(data.val());
-	}
-	function errData(err) {
-		console.log("Erreur !");
-		console.log(err);
-	}
+	    function gotData(data) {
+            console.log(data.val());
+            var citations = data.val();
+            var keys = Object.keys(citations)
+            console.log(keys)
+            var length = keys.length
+            var idd = Math.floor(Math.random() * length);
+            var id = idd - 1
+            var k = keys[0]
+            console.log(citations[k]);
+            var citiation = citations[k].citation;
+            var aut = citations[k].auteur;
+            const embed = new RichEmbed()
+	        .setColor("#D9F200")
+	        .setImage("https://omnilogie.fr/images/O/e239ced74cfc679e987778a89a95ebe0.jpg")
+	        .setTitle("Citation :")
+	        .setDescription(`${citiation}\nde ${aut}, ajoutée par WIP à la base de donnée.`);
+            msg.embed(embed);
+	    }
+	    function errData(err) {
+		    console.log("Erreur !");
+		    console.log(err);
+	    }
         
-        //const embed = new RichEmbed()
-	   //.setColor("#D9F200")
-	   //.setImage("https://omnilogie.fr/images/O/e239ced74cfc679e987778a89a95ebe0.jpg")
-	   //.setTitle("Citation :")
-	  // .setDescription(`${citiation}\nde ${aut}, ajoutée par WIP à la base de donnée.`);
-        //msg.embed(embed);
+        
     }};
