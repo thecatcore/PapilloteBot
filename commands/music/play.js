64ef3d9f4c800dfbb3de4091ec96644798757b03
Arthur Roussel <arthur.koalas@free.fr>
2018-03-22 16:36:23 +0100

    let's testing the command !

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,5 +1,18 @@
 const { Command } = require("discord.js-commando");
 const { RichEmbed } = require("discord.js");
+const YTDL = require("ytdl-core");
+
+var servers = {}
+
+function play(connection, message) {
+    var server = servers[msg.guild.id];
+    server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}));
+    server.queue.shift();
+    server.dispatcher.on("end", function() {
+        if (server.queue[0]) play(connection, message);
+        else connection.disconnect();
+    })
+}
 
 module.exports = class PlayCommand extends Command {
     constructor(client) {
@@ -21,5 +34,16 @@ module.exports = class PlayCommand extends Command {
     }
 
     run(msg, { linkname }) {
+        if (!msg.member.voiceChannel) {
+            return msg.say("Tu dois être dans un channel vocal !")
+        };
+        if (!servers[msg.guild.id]) servers[msg.guild.id] = {
+            queue: []
+        };
+        var server = servers[msg.guild.id]
+
+        if (!msg.guild.voiceConnection) msg.member.voiceChannel.join().then(function(connection) {
+            play(connection, message)
+        });
     }
 };
