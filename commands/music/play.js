bb21d22f1a7eee40e9db0be04968cc4dbbb5f72a
Arthur Roussel <arthur.koalas@free.fr>
2018-03-26 18:52:48 +0200

    I test a things

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,6 +1,20 @@
 const { Command } = require("discord.js-commando");
 const { RichEmbed } = require("discord.js");
 
+var guilds = {};
+
+if (!guilds[msg.guild.id]) {
+    guilds[msg.guild.id] = {
+        queue: [],
+        queueNames: [],
+        isPlaying: false,
+        dispatcher: null,
+        voiceChannel: null,
+        skipReq: 0,
+        skippers: []
+    }
+}
+
 module.exports = class PlayCommand extends Command {
     constructor(client) {
         super(client, {
@@ -21,13 +35,91 @@ module.exports = class PlayCommand extends Command {
     }
 
     run(msg, { linkname }) {
-        //if (!msg.member.voiceChannel) {
-          //  return msg.say("Tu dois être dans un channel vocal !")
-        //};
-        //var link = new String(linkname)
-        //if (!msg.guild.voiceConnection) msg.member.voiceChannel.join().then(function(connection) {
-          //  connection.playArbitraryInput(link.toString());
-           // return msg.say("Vous écoutez ce lien : " + linkname);
-        //});
+        if (guilds[msg.guild.id].queue.length > 0 || guilds[msg.guild.id].isPlaying) {
+            getID(linkname, function (id) {
+                add_to_queue(id, msg);
+                fetchVideoInfo(id, function(err, videoInfo) {
+                    if (err) throw new Error(err);
+                    msg.say(" Ajoutée à la queue : " + videoInfo.title);
+                    guilds[msg.guild.id].queueNames.push(videoInfo.title)
+                })
+            });
+        } else {
+            guilds[msg.guild.id].isPlaying = true;
+            getID(linkname, function(id) {
+                guilds[msg.guild.id].queue.push("placeholder");
+                playMusic(id, msg);
+                fetchVideoInfo(id, function(err, videoInfo) {
+                    if (err) throw new Error(err);
+                    console.log(videoInfo)
+                    msg.say(" Joue maintenant : " + videoInfo.title);
+                    guilds[msg.guild.id].queueNames.push(videoInfo.title)
+                })
+            })
+        }
     }
 };
+
+function playMusic(id, msg) {
+    guilds[msg.guild.id].voiceChannel = msg.member.voiceChannel;
+    console.log(msg.member)
+    console.log(msg.member.GuildMember)
+    console.log(guilds[msg.guild.id].voiceChannel)
+    guilds[msg.guild.id].voiceChannel.join().then(function (connection) {
+        stream = ytdl("https://www.youtube.com/watch?v=" + id, {
+            filter: "audioonly"
+        });
+        guilds[msg.guild.id].skipReq = 0;
+        guilds[msg.guild.id].skippers = [];
+
+        guilds[msg.guild.id].dispatcher = connection.playStream(stream);
+        guilds[msg.guild.id].dispatcher.on("end", function () {
+            guilds[msg.guild.id].skipReq = 0;
+            guilds[msg.guild.id].skippers = [];
+            guilds[msg.guild.id].queue.shift();
+            guilds[msg.guild.id].queueNames.shift()
+            if (guilds[msg.guild.id].queue.length === 0) {
+                guilds[msg.guild.id].queue = [];
+                guilds[msg.guild.id].queueNames = [];
+                guilds[msg.guild.id].isPlaying = false;
+            } else {
+                setTimeout(function () {
+                    playMusic(guilds[msg.guild.id].queue[0], msg)
+                }, 500)
+            }
+        })
+    }).catch(console.error);
+}
+
+function getID(str, cb) {
+    if (isYoutube(str)) {
+        cb(getYouTubeID(str));
+    } else {
+        search_video(str, function (id) {
+            cb(id);
+        })
+    }
+}
+
+function add_to_queue(strID, msg) {
+    if (isYoutube(strID)) {
+        guilds[msg.guild.id].queue.push(getYouTubeID(strID));
+    } else {
+        guilds[msg.guild.id].queue.push(strID);
+    }
+}
+
+function search_video(query, callback) {
+    request("https://www.googleapis.com/youtube/v3/search?part=id&type=video&q=" + encodeURIComponent(query) + "&key=" + process.env.YT_KEY, function(error, response, body) {
+       var json = JSON.parse(body);
+       console.log(json);
+       if (!json.items[0]) callback("3_-a9nVZYjk");
+       else {
+        callback(json.items[0].id.videoId)
+       }
+    })
+}
+
+function isYoutube(str) {
+    return str.toLowerCase().indexOf("youtube.com") > -1;
+}
