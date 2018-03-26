1fb48115e754ce9fea84b653f063bad9c3fb1c26
Arthur Roussel <arthur.koalas@free.fr>
2018-03-26 19:27:05 +0200

    add skip command

---
+++ commands/music/skip.js
@@ -0,0 +1,39 @@
+const { Command } = require("discord.js-commando");
+const { RichEmbed } = require("discord.js");
+const ytdl = require("ytdl-core");
+const request = require("request");
+const getYouTubeID = require("get-youtube-id");
+const fetchVideoInfo = require("youtube-info");
+import guilds from "./play"
+
+module.exports = class QueueCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: "queue",
+            aliases: ["q"],
+            memberName: "queue",
+            group : "music",
+            description: "",
+            examples: [""]
+        });    
+    }
+
+    run(msg) {
+        if (guilds[message.guild.id].skippers.indexOf(message.author.id) === -1) {
+            guilds[message.guild.id].skippers.push(message.author.id);
+            guilds[message.guild.id].skipReq++;
+            skip_song(message);
+            message.reply("Skip en cours !")
+        }
+    }
+};
+
+function skip_song(message) {
+    guilds[message.guild.id].dispatcher.end();
+    if (guilds[message.guild.id].queue.length > 1) {
+        playMusic(guilds[message.guild.id].queue[0].message);
+    } else {
+        guilds[message.guild.id].skipReq = 0;
+        guilds[message.guild.id].skippers = [];
+    }
+}
