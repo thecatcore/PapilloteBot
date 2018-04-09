632c1308a7c6ab7dd05c8158b52d70f666218161
Arthur Roussel <arthur.koalas@free.fr>
2018-04-09 15:33:38 +0200

    start of bot

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
+const Discord = require("discord.js")
+const client = new Discord.Client()
+const fs = require("fs")
+const Enmap = require("enmap")
 
+
+
+client.config = require("./config.json")
+client.login(client.config.token)
 
 // const Commando = require("discord.js-commando");
 // const path = require("path");
