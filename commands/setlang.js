1ad77b538afd4075d48b4ed93629b56b206029a7
arthurbambou <34719527+arthurbambou@users.noreply.github.com>
2018-04-24 10:58:11 +0000

    Update setlang.js

--- commands/setlang.js
+++ commands/setlang.js
@@ -1,10 +1,3 @@
-const firebase = require("firebase");
-var config = {
-    apiKey: "AIzaSyBS7yIZr45Y1yrWBalCpO3Y2bkS5OYJQQY",
-    authDomain: "papillotebot.firebaseapp.com",
-    databaseURL: "https://papillotebot.firebaseio.com",
-    projectId: "papillotebot",
-    storageBucket: "papillotebot.appspot.com",
-    messagingSenderId: "68194330678"
-};
-firebase.initializeApp(config);
+exports.run = (client, message, [lang]) => {
+    
+}
