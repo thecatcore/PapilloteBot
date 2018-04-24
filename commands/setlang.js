db53b7dab9ddbee0bd28ce6c8e222aede1777165
arthurbambou <34719527+arthurbambou@users.noreply.github.com>
2018-04-24 10:56:57 +0000

    Create setlang.js

---
+++ commands/setlang.js
@@ -0,0 +1,10 @@
+const firebase = require("firebase");
+var config = {
+    apiKey: "AIzaSyBS7yIZr45Y1yrWBalCpO3Y2bkS5OYJQQY",
+    authDomain: "papillotebot.firebaseapp.com",
+    databaseURL: "https://papillotebot.firebaseio.com",
+    projectId: "papillotebot",
+    storageBucket: "papillotebot.appspot.com",
+    messagingSenderId: "68194330678"
+};
+firebase.initializeApp(config);
