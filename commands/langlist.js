5f47290a82eade329620854441e0f05e2d8cf103
arthurbambou <34719527+arthurbambou@users.noreply.github.com>
2018-04-24 11:08:15 +0000

    Update langlist.js

--- commands/langlist.js
+++ commands/langlist.js
@@ -1,1 +1,23 @@
+const serverlang = require(`./langs/${server.lang}`)
+msg.channel.send(serverlang.langlist)
+fs.readdir("./langs/", (err, files) => {
 
+    if (err) {
+
+        return console.error(err)
+
+    }
+    files.forEach((file) => {
+
+        if (!file.endsWith(".json")) {
+
+            return
+
+        }
+        let langName = file.split(".")[0]
+
+        msg.channel.send(langName);
+
+    })
+
+})
