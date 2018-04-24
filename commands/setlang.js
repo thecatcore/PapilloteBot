e7f381771d45daa341c31555314d8248e1198575
arthurbambou <34719527+arthurbambou@users.noreply.github.com>
2018-04-24 11:23:04 +0000

    Update setlang.js

--- commands/setlang.js
+++ commands/setlang.js
@@ -1,3 +1,10 @@
-exports.run = (client, message, [lang]) => {
-    
+const firebase = require("firebase")
+module.exports = (client, member, [lang]) => {
+    const guild = member.guild;
+    var database = firebase.database();
+    var ref = database.ref(`server/${guild.id}`);
+    var lang = {
+            lang: lang
+    };
+    ref.set(lang);
 }
