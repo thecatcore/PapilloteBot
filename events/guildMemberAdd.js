036b9238fd71b0b40442f49e9450c708c71cbab8
arthurbambou <34719527+arthurbambou@users.noreply.github.com>
2018-04-24 11:18:20 +0000

    add Default Settings to server

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js")
 const newUsers = [];
+const firebase = require("firebase")
 module.exports = (client, member) => {
     const guild = member.guild;
     if (!newUsers[guild.id]) newUsers[guild.id] = new Discord.Collection();
@@ -7,4 +8,10 @@ module.exports = (client, member) => {
     const userlist = newUsers[guild.id].map(u => u.toString()).join(" ");
     guild.channels.find("name", "general").send("Bienvenue sur le serveur !\n" + userlist);
     newUsers[guild.id].clear();
+    var database = firebase.database();
+    var ref = database.ref(`server/${guild.id}`);
+    var defaultSettings = {
+            lang: "en_US"
+    };
+    ref.set(defaultSettings);
 }
