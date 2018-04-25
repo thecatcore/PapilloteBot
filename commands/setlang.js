3ac747a5eba43ceb70dec9bfccb5588ece791203
Arthur Roussel <arthur.koalas@free.fr>
2018-04-25 17:33:22 +0200

    Fix error

--- commands/setlang.js
+++ commands/setlang.js
@@ -1,5 +1,5 @@
 const firebase = require("firebase")
-module.exports = (client, member, [lang]) => {
+exports.run = (client, member, [lang]) => {
     const guild = member.guild;
     var database = firebase.database();
     var ref = database.ref(`server/${guild.id}`);
