585813eb450f6eccea6d80b3d9984f04aefd1d61
Arthur Roussel <arthur.koalas@free.fr>
2018-05-01 13:48:21 +0200

    add recette link to db

--- events/message.js
+++ events/message.js
@@ -1,3 +1,5 @@
+const Discord = require("discord.js")
+
 module.exports = (client, message) => {
   // Ignore all bots
   if (message.author.bot) {
@@ -9,7 +11,42 @@ module.exports = (client, message) => {
   }
   var ref = client.db.ref("recette")
   function gotData(data) {
-    console.log(data);
+    var recette = data.val();
+    var keys = Object.keys(recette);
+    var length = keys.length;
+    for (i=0;i < length;i++) {
+      var k = keys[i];
+      if (message.content = client.config.prefix + recette[k].name) {
+        var recettes = recette[k]
+        var nom = recettes.name
+        var ingrédients = recettes.ingrédients
+        var étapes = recettes.étapes
+        var étapess = Object.keys(étapes)
+        var étapesLength = étapess.length
+        var recetteEmbed = new Discord.RichEmbed()
+          .setTitle(nom)
+          .addField("Ingrédients", ingrédients);
+        if (recettes.image) {
+          recetteEmbed.setImage(recettes.image)
+        }
+        for (var e = 0;e < étapesLength; e++) {
+          var é = étapess[e]
+          var f = e + 1
+          recetteEmbed.addField(`Étape ${f}: `, étapes[é].description)
+        }
+        if (recettes.options) {
+          var options = recettes.options
+          var optionss = Object.keys(options)
+          var optionsLength = optionss.length
+          for (var r = 0;r < optionsLength; r++) {
+            var t = optionss[r]
+            recetteEmbed.addField(`Option ${options[t].name}: `, options[t].ingrédients)
+          }
+        }
+        console.log(recetteEmbed)
+        message.channel.send(recetteEmbed)
+      }
+    }
   }
   function errData(err) {
     console.log(err);
