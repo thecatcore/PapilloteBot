9b2e6ab756ab3bacc22904b1cdcb5a31291f8926
Arthur Roussel <arthur.koalas@free.fr>
2018-05-01 13:18:30 +0200

    testing db

--- commands/db.js
+++ commands/db.js
@@ -1,11 +1,7 @@
 exports.run = (client) => {
     var ref = client.db.ref("recette")
-    ref.push({
-        name:"gâteau au chocolat",
-        ingrédients:"100g de beurre\n100g de chocolat\n3 oeufs entiers\n100g de sucre\n60g de farine"
-    })
     function gotData(data) {
-        console.log(data);
+        console.log(data.val());
     }
     function errData(err) {
         console.log(err);
