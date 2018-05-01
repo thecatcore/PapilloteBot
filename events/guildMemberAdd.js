10d6dd2dba0b8c4dfbd2f4f0bc28b93af0896ee7
Arthur Roussel <arthur.koalas@free.fr>
2018-05-01 12:03:04 +0200

    Fix guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,10 +1,12 @@
-const Discord = require("discord.js")
+const Discord = require("discord.js");
 const newUsers = [];
-const firebase = require("firebase")
+const firebase = require("firebase");
 module.exports = (client, member) => {
     const guild = member.guild;
-    if (!newUsers[guild.id]) newUsers[guild.id] = new Discord.Collection();
-        newUsers[guild.id].set(member.id, member.user);
+    if (!newUsers[guild.id]) {
+        newUsers[guild.id] = new Discord.Collection();
+    }
+    newUsers[guild.id].set(member.id, member.user);
     const userlist = newUsers[guild.id].map(u => u.toString()).join(" ");
     guild.channels.find("name", "general").send("Bienvenue sur le serveur !\n" + userlist);
     newUsers[guild.id].clear();
@@ -14,4 +16,4 @@ module.exports = (client, member) => {
     //         lang: "en_US"
     // };
     // ref.set(defaultSettings);
-}
+};
