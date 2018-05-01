dabdeb08d235de6e1057bd0d103a9002468a1d2d
Arthur Roussel <arthur.koalas@free.fr>
2018-05-01 12:07:42 +0200

    Fix kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,17 +1,17 @@
 exports.run = (client, message, [mention, ...reason]) => {
     const modRole = message.guild.roles.find("name", "Mods");
-    if (!modRole)
+    if (!modRole) {
       return message.reply("Le rôle modérateur n'existe pas !");
-  
-    if (!message.member.roles.has(modRole.id))
+    }
+    if (!message.member.roles.has(modRole.id)) {
       return message.reply("Vous n'avez pas la permission d'utiliser cette commande !");
-  
-    if (message.mentions.members.size === 0)
+    }
+    if (message.mentions.members.size === 0) {
       return message.reply("Veuillez mentionner la personne à kick");
-  
-    if (!message.guild.me.hasPermission("KICK_MEMBERS"))
+    }
+    if (!message.guild.me.hasPermission("KICK_MEMBERS")) {
       return message.reply("Vous n'avez pas la permission d'utiliser cette commande !");
-  
+    }
     const kickMember = message.mentions.members.first();
   
     kickMember.kick(reason.join(" ")).then(member => {
