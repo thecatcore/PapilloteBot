bdfe1668ce55a24aa52d628544b3b29f2ac598da
Arthur Roussel <arthur.koalas@free.fr>
2018-05-01 11:55:23 +0200

    Fix langlist.js

--- commands/langlist.js
+++ commands/langlist.js
@@ -1,24 +1,24 @@
-const fs = require("fs")
-const firebase = require("firebase")
+const fs = require("fs");
+const firebase = require("firebase");
 exports.run = (client, msg) => {
 const guild = msg.guild;
 var database = firebase.database();
 var ref = database.ref(`server/${guild.id}/lang`);
 function gotData(data) {
     var serverlang = data.node_.value_;
-    var lang = database.ref(`langs/${serverlang}/langlist`)
+    var lang = database.ref(`langs/${serverlang}/langlist`);
     function gotDataa(dataa) {
-        var langlist = dataa.node_.value_
-        msg.channel.send(`${langlist}\nen_US\nfr_FR`)
+        var langlist = dataa.node_.value_;
+        msg.channel.send(`${langlist}\nen_US\nfr_FR`);
     }
     function errDataa(erra) {
-        msg.channel.send(`Erreur: ${erra}`)
+        msg.channel.send(`Erreur: ${erra}`);
     }
-    lang.on("value", gotDataa, errDataa)
+    lang.on("value", gotDataa, errDataa);
 }
 function errData(err) {
-    msg.channel.send(`Erreur: ${err}`)
+    msg.channel.send(`Erreur: ${err}`);
 }
 
 ref.on("value", gotData, errData);
-}
+};
