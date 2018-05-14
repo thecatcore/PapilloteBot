609370c5a620f7addaa51f0ecac43ec687a3a406
Arthur Roussel <arthur.koalas@free.fr>
2018-05-14 19:33:59 +0200

    SPace

--- commands/db.js
+++ commands/db.js
@@ -1,11 +1,11 @@
 exports.run = (client) => {
-    var ref = client.db.ref("recette/-LBQZb9IOU52GVIIGkqs/étapes")
-    function gotData(data) {
-        console.log(data.val());
-    }
-    function errData(err) {
-        console.log(err);
-    }
+    // var ref = client.db.ref("recette/-LBQZb9IOU52GVIIGkqs/étapes")
+    // function gotData(data) {
+    //     console.log(data.val());
+    // }
+    // function errData(err) {
+    //     console.log(err);
+    // }
     
-    ref.on("value", gotData, errData);
+    // ref.on("value", gotData, errData);
 }
