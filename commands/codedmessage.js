7d9c7adb25ff0b6535fe7d66349884825845f495
arthurbambou <34719527+arthurbambou@users.noreply.github.com>
2018-05-15 12:53:32 +0200

    Update codedmessage.js

--- commands/codedmessage.js
+++ commands/codedmessage.js
@@ -1,2 +1,9 @@
 exports.run = (client, message, [type, number, ...texttocode]) => {
+  if (type=="server") {
+    if (number==undifined) return message.reply(numbererror)
+  } else if (type=="dm") {
+    if (number==undifined) return message.reply(numbererror)
+  } else {
+    return message.reply(typeerror)
+  }
 }
