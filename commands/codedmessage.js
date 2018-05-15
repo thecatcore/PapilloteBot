3660c57698c10a77b49101f33fffe97fa89dc5cc
arthurbambou <34719527+arthurbambou@users.noreply.github.com>
2018-05-15 13:29:49 +0200

    Update codedmessage.js

--- commands/codedmessage.js
+++ commands/codedmessage.js
@@ -1,8 +1,18 @@
-const alphabet = require("./tools/alphabet.json")
 exports.run = (client, message, [type, number, ...texttocode]) => {
   if (type=="server") {
     if (number==undifined) return message.reply(numbererror)
-    
+    var a;
+    var b;
+    var texttocoded = texttocode.split("")
+    var textcoding = ""
+    for (b=0;b < texttocode.length;b++) {
+      const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    for (a=0;a < alphabet.length;a++)
+      if (texttocoded[b] == alphabet[a]) {
+        var textcoding = textcoding + texttocoded[b]
+      }
+    }
+    }
   } else if (type=="dm") {
     if (number==undifined) return message.reply(numbererror)
   } else {
