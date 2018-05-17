443dd1d20e9667aad429ab675bc88a76b48dcd13
arthurbambou <34719527+arthurbambou@users.noreply.github.com>
2018-05-17 11:36:05 +0200

    Update codedmessage.js

--- commands/codedmessage.js
+++ commands/codedmessage.js
@@ -4,15 +4,31 @@ exports.run = (client, message, [type, number, ...texttocode]) => {
     var a;
     var b;
     var texttocoded = texttocode.split("")
+    console.log(texttocoded)
     var textcoding = ""
     for (b=0;b < texttocode.length;b++) {
       const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-    for (a=0;a < alphabet.length;a++)
-      if (texttocoded[b] == alphabet[a]) {
-        var textcoding = textcoding + texttocoded[b]
+      var oletter;
+    for (a=0;a < alphabet.length;a++) {
+      oletter = texttocoded[b];
+      console.log(oletter)
+      var cletter
+      var cletternum
+      var oletternum
+      if (oletter == alphabet[a]) {
+        oletternum = a;
+        console.log(oletternum);
+        cletternum = oletternum + number;
+        console.log(oletternum + "->" + cletternum);
+        cletter = alphabet[cletternum];
+        console.log(oletter + "->" + cletter);
+        textcoding = textcoding + cletter;
+        console.log(textcoding)
       }
     }
     }
+    var resultembed = new client.RichEmbed()
+      .settitle("Voici votre message codé)
   } else if (type=="dm") {
     if (number==undifined) return message.reply(numbererror)
   } else {
