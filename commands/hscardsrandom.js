6f3c44e154154c6b19b5f5067ad60598c80f18cc
arthurbambou <34719527+arthurbambou@users.noreply.github.com>
2018-05-18 10:46:53 +0200

    Create hscardsrandom.js

---
+++ commands/hscardsrandom.js
@@ -0,0 +1,26 @@
+var random = Math.floor(Math.random() * cards.length);
+        var card = cards[random];
+        var packet;
+        if (card.collectible === true) {
+            packet = "oui";
+        } else {
+            packet = "non";
+        }
+        console.log("hearthstone cards random Command");
+        var embed = new RichEmbed()
+            .setTitle("Carte aléatoire")
+            .addField("Nom", card.name)
+            .addField("Coût", card.cost)
+            .addField("Attack", card.attack)
+            .addField("PV", card.health)
+            .addField("Rareté", card.rarity)
+            .addField("Texte", card.text)
+            .addField("Description dans la collection", card.flavor)
+            .addField("Artiste", card.artist)
+            .addField("Classe", card.cardClass)
+            .addField("Race", card.race)
+            .addField("Type", card.type)
+            .addField("ID texte", card.id)
+            .addField("ID nombre", card.dbfId)
+            .addField("Collectionable", packet);
+return msg.embed(embed);
