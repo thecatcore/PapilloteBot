d06ed2283ccc893f8548324ec5c748ab0bcbc588
arthurbambou <34719527+arthurbambou@users.noreply.github.com>
2018-05-18 10:34:46 +0200

    Update dog.js

--- commands/dog.js
+++ commands/dog.js
@@ -1,2 +1,15 @@
-exports.run = (client, message, [type, number, ...texttocode]) => {
+const superagent = require("superagent")
+const { RichEmbed } = require("discord.js")
+exports.run = (client, msg) => {
+  async run(msg) {
+     const { body } = await superagent
+        .get("https://dog.ceo/api/breeds/image/random");
+	   const embed = new RichEmbed()
+	   .setColor(0x954D23)
+	   .setTitle("Woof :dog:")
+	   .setImage(body.message);
+     return msg.channel.send(embed);
+        //msg.channel.send("https://random.dog");
+}
+  run();
 }
