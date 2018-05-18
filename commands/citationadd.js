4b3b29d774e884b5bebb3deed84fb8905a834149
arthurbambou <34719527+arthurbambou@users.noreply.github.com>
2018-05-18 10:42:42 +0200

    Update citationadd.js

--- commands/citationadd.js
+++ commands/citationadd.js
@@ -1,2 +1,21 @@
-exports.run = (client, message, [type, number, ...texttocode]) => {
+const firebase = require("firebase");
+var database = firebase.database();
+var ref = database.ref("citations");
+exports.run = (client, message, [auteur, ...citation]) => {
+  function gotData (data) {
+
+        }
+        function errData (err) {
+            
+        }
+
+        ref.on("value", gotData, errData);
+        msg.channel.send(`Ajout de la citation:\n${citation}\nde ${auteur} par ${message.author} à la base de données.`);
+        var cont = new String(message.author);
+        var data = {
+            citation: citation,
+            auteur: auteur,
+            contributeur: cont.toString()
+        };
+        ref.push(data);
 }
