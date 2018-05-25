7d18f120038f9e7bcfa683e70e5fe50f10a687c5
Arthur Roussel <arthur.koalas@free.fr>
2018-05-25 16:54:53 +0200

    Fix citationadd command

--- commands/citationadd.js
+++ commands/citationadd.js
@@ -1,7 +1,7 @@
 const firebase = require("firebase");
 var database = firebase.database();
 var ref = database.ref("citations");
-exports.run = (client, message, [auteur, ...citation]) => {
+exports.run = (client, message, [auteur, ...citiation]) => {
   function gotData (data) {
 
         }
@@ -10,6 +10,11 @@ exports.run = (client, message, [auteur, ...citation]) => {
         }
 
         ref.on("value", gotData, errData);
+        var citation = "";
+        for (a=0;a < citiation.length; a++) {
+            citation = citation + " " + citiation[a]
+        }
+        console.log(citation)
         message.channel.send(`Ajout de la citation:\n${citation}\nde ${auteur} par ${message.author} à la base de données.`);
         var cont = new String(message.author);
         var data = {
