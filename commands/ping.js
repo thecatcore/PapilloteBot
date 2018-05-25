bb35e584d8fe075702094c02fbfe301cbfa466cf
Arthur Roussel <arthur.koalas@free.fr>
2018-05-25 17:14:31 +0200

    Update ping command

--- commands/ping.js
+++ commands/ping.js
@@ -1,3 +1,4 @@
-exports.run = (client, message, args) => {
-    message.channel.send("pong!").catch(console.error);
+exports.run = (client, message) => {
+    message.delete();
+    message.channel.send('Ping?').then(m => m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`) );
 };
