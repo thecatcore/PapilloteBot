be5c5699aacf70dd61160669238200304d2215cc
arthurbambou <34719527+arthurbambou@users.noreply.github.com>
2018-05-28 13:18:04 +0200

    Fix ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,4 +1,4 @@
 exports.run = (client, message) => {
     message.delete();
-    message.channel.send('Ping?').then(m => m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`) );
+    message.channel.send("Ping?").then((m) => m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`) );
 };
