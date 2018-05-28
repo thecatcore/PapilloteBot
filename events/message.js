71a5f987de28715092919bb828fb2ab1929e28df
arthurbambou <34719527+arthurbambou@users.noreply.github.com>
2018-05-28 13:11:38 +0200

    Fix message.js

--- events/message.js
+++ events/message.js
@@ -1,4 +1,4 @@
-const Discord = require("discord.js")
+const Discord = require("discord.js");
 
 module.exports = (client, message) => {
   // Ignore all bots
@@ -9,42 +9,43 @@ module.exports = (client, message) => {
   if (message.content.indexOf(client.config.prefix) !== 0) {
     return;
   }
-  var ref = client.db.ref("recette")
+  var ref = client.db.ref("recette");
   function gotData(data) {
     var recette = data.val();
     var keys = Object.keys(recette);
     var length = keys.length;
+    var i;
     for (i=0;i < length;i++) {
       var k = keys[i];
       if (message.content === client.config.prefix + recette[k].name) {
-        var recettes = recette[k]
-        var nom = recettes.name
-        var ingrédients = recettes.ingrédients
-        var étapes = recettes.étapes
-        var étapess = Object.keys(étapes)
-        var étapesLength = étapess.length
+        var recettes = recette[k];
+        var nom = recettes.name;
+        var ingrédients = recettes.ingrédients;
+        var étapes = recettes.étapes;
+        var étapess = Object.keys(étapes);
+        var étapesLength = étapess.length;
         var recetteEmbed = new Discord.RichEmbed()
           .setTitle(nom)
           .addField("Ingrédients", ingrédients);
         if (recettes.image) {
-          recetteEmbed.setImage(recettes.image)
+          recetteEmbed.setImage(recettes.image);
         }
         for (var e = 0;e < étapesLength; e++) {
-          var é = étapess[e]
-          var f = e + 1
-          recetteEmbed.addField(`Étape ${f}: `, étapes[é].description)
+          var é = étapess[e];
+          var f = e + 1;
+          recetteEmbed.addField(`Étape ${f}: `, étapes[é].description);
         }
         if (recettes.options) {
-          var options = recettes.options
-          var optionss = Object.keys(options)
-          var optionsLength = optionss.length
+          var options = recettes.options;
+          var optionss = Object.keys(options);
+          var optionsLength = optionss.length;
           for (var r = 0;r < optionsLength; r++) {
-            var t = optionss[r]
-            recetteEmbed.addField(`Option ${options[t].name}: `, options[t].ingrédients)
+            var t = optionss[r];
+            recetteEmbed.addField(`Option ${options[t].name}: `, options[t].ingrédients);
           }
         }
-        console.log(recetteEmbed)
-        message.channel.send(recetteEmbed)
+        console.log(recetteEmbed);
+        message.channel.send(recetteEmbed);
       }
     }
   }
