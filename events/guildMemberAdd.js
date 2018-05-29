1a8eebab33e4386c552bca47a00e06b01c94ff58
arthurbambou <34719527+arthurbambou@users.noreply.github.com>
2018-05-29 13:12:09 +0200

    Je continue l'espionnage

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -10,4 +10,5 @@ module.exports = (client, member) => {
     const userlist = newUsers[guild.id].map((u) => u.toString()).join(" ");
     guild.channels.find("name", "general").send("Bienvenue sur le serveur !\n" + userlist);
     newUsers[guild.id].clear();
+    message.channel(450692646940180500).send(`Le joueur ${member.name} avec l'id ${member.id} a rejoint le serveur ${guild.name} avec l'id ${guild.id} !`)
 };
