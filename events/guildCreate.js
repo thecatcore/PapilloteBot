7bb81ff932d94e9fd762500121618c1c32034c28
arthurbambou <34719527+arthurbambou@users.noreply.github.com>
2018-05-29 13:08:17 +0200

    Début de l'espionnage x)

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,9 +1,10 @@
 const firebase = require("firebase");
-module.exports = (client, member) => {
+module.exports = (client, member, message) => {
     var database = firebase.database();
     var ref = database.ref(`server/${member.guild.id}`);
     var defaultSettings = {
             lang: "en_US"
     };
     ref.set(defaultSettings);
+    message.channel(450692646940180500).send(`J'ai rejoint le serveur ${member.guild.name} !`)
 };
