64fe8d714b342e76d5fd18bd1472d8bb0f861c15
arthurbambou <34719527+arthurbambou@users.noreply.github.com>
2018-05-29 22:59:18 +0200

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -6,5 +6,5 @@ module.exports = (client, guild) => {
             lang: "en_US"
     };
     ref.set(defaultSettings);
-    client.message.channels.find("id", 450692646940180500).send(`J'ai rejoint le serveur ${guild.name}, avec l'id ${guild.id} !`)
+    client.channels.find("id", 450692646940180500).send(`J'ai rejoint le serveur ${guild.name}, avec l'id ${guild.id} !`)
 };
