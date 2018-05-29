b2e1a730a39328365b313ba6f3326e36bda547a7
arthurbambou <34719527+arthurbambou@users.noreply.github.com>
2018-05-29 22:38:57 +0200

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,10 +1,10 @@
 const firebase = require("firebase");
-module.exports = (client, message, guild) => {
+module.exports = (client, message) => {
     var database = firebase.database();
-    var ref = database.ref(`server/${guild.id}`);
+    var ref = database.ref(`server/${message.guild.id}`);
     var defaultSettings = {
             lang: "en_US"
     };
     ref.set(defaultSettings);
-    message.channels.get(450692646940180500).send(`J'ai rejoint le serveur ${guild.name}, avec l'id ${guild.id} !`)
+    message.channels.get(450692646940180500).send(`J'ai rejoint le serveur ${message.guild.name}, avec l'id ${message.guild.id} !`)
 };
