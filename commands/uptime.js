0e226990690ba6c1f21b2edc0a9982e72962a92a
arthurbambou <34719527+arthurbambou@users.noreply.github.com>
2018-05-29 12:57:57 +0200

    Update uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -1,4 +1,8 @@
-exports.run = (client) => {
-  client.uptime //ça existe !!!
-  Math.floor(5.99) //Math.floor(5.99) = 5
+exports.run = (client, message) => {
+  var ms = client.uptime
+  var seconde = Math.floor(ms/1000)
+  var minute = Math.floor(seconde/60)
+  var heure = Math.floor(minute/60)
+  var jour = Math.floor(heure/24)
+  message.channel.send(`Le bot est en ligne depuis ${ms} millisecondes, ${seconde} secondes, ${minute} minutes, ${heure} heures et ${jour} jour(s)`)
 }
