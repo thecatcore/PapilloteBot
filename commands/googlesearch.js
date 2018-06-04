a0b7b9afbadc0594bcd6092850aed0a007c08e84
arthurbambou <34719527+arthurbambou@users.noreply.github.com>
2018-06-04 13:20:06 +0200

    Create googlesearch.js

---
+++ commands/googlesearch.js
@@ -0,0 +1,48 @@
+exports.run = (client, message, ...search) => {/*
+    Example By Nomsy#7453
+    
+    This will require knowledge of ES7's await/async functions found in node 7+
+
+    REMINDER: <Message> is what you defined in the <Client>.on('<Message>', <Message>) event.
+*/
+var args = ""
+for (var c = 0; c < search.length; c++) {
+  args = args + " " + search[c]
+}
+
+// The modules we are using are cheerio, snekfetch, and querystring for this.
+const cheerio = require('cheerio'),
+      snekfetch = require('snekfetch'),
+      querystring = require('querystring');
+
+// Depending on your command framework (or if you use one), it doesn't have to
+// edit messages so you can rework it to fit your needs. Again, this doesn't have
+// to be async if you don't care about message editing.
+async function googleCommand(msg, args) {
+
+   // These are our two variables. One of them creates a message while we preform a search,
+   // the other generates a URL for our crawler.
+   let searchMessage = await <Message>.reply('Searching... Sec.');
+   let searchUrl = `https://www.google.com/search?q=${encodeURIComponent(msg.content)}`;
+
+   // We will now use snekfetch to crawl Google.com. Snekfetch uses promises so we will
+   // utilize that for our try/catch block.
+   return snekfetch.get(searchUrl).then((result) => {
+
+      // Cheerio lets us parse the HTML on our google result to grab the URL.
+      let $ = cheerio.load(result.text);
+
+      // This is allowing us to grab the URL from within the instance of the page (HTML)
+      let googleData = $('.r').first().find('a').first().attr('href');
+ 
+      // Now that we have our data from Google, we can send it to the channel.
+      googleData = querystring.parse(googleData.replace('/url?', ''));
+      searchMessage.edit(`Result found!\n${googleData.q}`);
+
+  // If no results are found, we catch it and return 'No results are found!'
+  }).catch((err) => {
+     searchMessage.edit('No results found!');
+  });
+}
+googleCommand(message, args);
+}
