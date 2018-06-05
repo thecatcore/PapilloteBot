112644e5df5631c5b8f2068c87e63b11e131e90d
arthurbambou <34719527+arthurbambou@users.noreply.github.com>
2018-06-05 12:56:37 +0200

    Fix uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -1,13 +1,13 @@
 exports.run = (client, message) => {
-  var mss = client.uptime
-  var secondee = Math.floor(mss/1000)
-  var minutee = Math.floor(secondee/60)
-  var heuree = Math.floor(minutee/60)
-  var jour = Math.floor(heuree/24)
-  var ms = mss - (secondee * 1000)
-  var seconde = secondee - (minutee * 60)
-  var minute = minutee - (heuree * 60)
-  var heure = heuree - (jour * 24)
-  console.log("ms : " + ms + " ou " + mss)
-  message.channel.send(`Le bot est en ligne depuis ${ms} millisecondes, ${seconde} secondes, ${minute} minutes, ${heure} heures et ${jour} jour(s)`)
-}
+  var mss = client.uptime;
+  var secondee = Math.floor(mss/1000);
+  var minutee = Math.floor(secondee/60);
+  var heuree = Math.floor(minutee/60);
+  var jour = Math.floor(heuree/24);
+  var ms = mss - (secondee * 1000);
+  var seconde = secondee - (minutee * 60);
+  var minute = minutee - (heuree * 60);
+  var heure = heuree - (jour * 24);
+  console.log("ms : " + ms + " ou " + mss);
+  message.channel.send(`Le bot est en ligne depuis ${ms} millisecondes, ${seconde} secondes, ${minute} minutes, ${heure} heures et ${jour} jour(s)`);
+};
