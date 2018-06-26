0016aba780c5f063c2629a42bed21971f282d82c
Arthur Roussel <arthur.koalas@free.fr>
2018-06-26 09:29:08 +0200

    L'espionage sur GuildCreate marche enfin xD

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,3 +1,4 @@
+const { RichEmbed } = require("discord.js");
 const firebase = require("firebase");
 module.exports = (client, guild) => {
     var database = firebase.database();
@@ -7,5 +8,19 @@ module.exports = (client, guild) => {
     };
     ref.set(defaultSettings);
     var chan = client.channels.get("450692646940180500")
-    chan.send(`J'ai rejoint le serveur ${guild.name}, avec l'id ${guild.id} !`)
+    var embedespion = new RichEmbed()
+        .setTitle("J'ai rejoint un nouveau serveur !")
+        .addField("Nom", guild.name)
+        .addField("ID", guild.id)
+        .addField("Nombre de membres", guild.members.size)
+        .addField("Il appartient à :", `${guild.owner.user.username}#${guild.owner.user.discriminator} qui a pour id : ${guild.owner.user.id}`)
+        .addField("Date de création", guild.createdAt)
+        .addField("Nombre d'émoji", guild.emojis.size)
+        .addField("Région", guild.region)
+        .addField("Nombre de connecté", guild.presences.size)
+        .addField("Nombre de Rôles", guild.roles.size)
+        .setThumbnail(guild.icon)
+        .setImage(guild.iconURL)
+    chan.send(embedespion)
+    console.log(guild)
 };
