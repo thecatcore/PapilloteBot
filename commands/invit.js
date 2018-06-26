e7dae617ca8a59ab1ac2c3726b48b0a2f5f75f36
Arthur Roussel <arthur.koalas@free.fr>
2018-06-26 09:28:03 +0200

    add a new command who is send an invite link for the bot

---
+++ commands/invit.js
@@ -0,0 +1,3 @@
+exports.run = (client, message) => {
+  message.channel.send("Voici le lien qui te permettra de m'inviter sur tes serveurs :-) : \nhttps://discordapp.com/oauth2/authorize?client_id=391292458077782016&permissions=8&scope=bot")
+};
